b77bf9dc79644233933eeb498c46e2f70c29f113
Josh Teperman <Josh.teperman@weployapp.com>
2021-06-05 09:43:47 +1000

    toggle checklist item

--- app/reflexes/task_reflex.rb
+++ app/reflexes/task_reflex.rb
@@ -1,6 +1,12 @@
 class TaskReflex < StimulusReflex::Reflex
-  def complete
-    require 'pry';binding.pry
-    Task.find(element.dataset.id).update!(completed_at: Time.current)
+  def toggle
+    id = element.id.split('_').last
+    task = Task.find(id)
+
+    if task.completed_at.present?
+      task.update!(completed_at: nil)
+    else
+      task.update!(completed_at: Time.current)
+    end
   end
 end
