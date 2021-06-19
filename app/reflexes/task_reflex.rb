63a124301598273aabd5d560509726c72033bc98
Josh Teperman <Josh.teperman@weployapp.com>
2021-06-19 16:54:54 +1000

    set button as target element when calling `stimuluate` so we can use a common set_task hook

--- app/reflexes/task_reflex.rb
+++ app/reflexes/task_reflex.rb
@@ -1,16 +1,21 @@
 class TaskReflex < StimulusReflex::Reflex
-  def toggle
-    task = Task.find(element.dataset.id)
+  before_reflex :set_task, only: [:toggle, :delete]
 
-    if task.completed_at.present?
-      task.update!(completed_at: nil)
+  def toggle
+    if @task.completed_at.present?
+      @task.update!(completed_at: nil)
     else
-      task.update!(completed_at: Time.current)
+      @task.update!(completed_at: Time.current)
     end
   end
 
-  def delete(id)
-    task = Task.find(id)
-    task.update(deleted_at: Time.current)
+  def delete
+    @task.update(deleted_at: Time.current)
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(element.dataset.id)
   end
 end
