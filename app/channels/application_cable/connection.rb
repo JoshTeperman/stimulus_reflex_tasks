7eb94d3dbb7d0b740cd3472362ae159dbe45b1f0
Josh Teperman <Josh.teperman@weployapp.com>
2021-06-21 08:53:25 +1000

    identify action cable connection by warden current_user

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = verified_user
+    end
+
+    protected
+
+    def verified_user
+      if (current_user = env['warden'].user)
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
