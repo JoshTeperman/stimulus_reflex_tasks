ddc4b84f5ebe85825c789113fdcb68c96cf70fa1
Josh Teperman <Josh.teperman@weployapp.com>
2021-06-20 14:42:19 +1000

    make tasks sortable with sortablejs

--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -1,7 +1,13 @@
 import ApplicationController from './application_controller';
+import Sortable from 'sortablejs';
 
 export default class extends ApplicationController {
-  static targets = ['form', 'input']
+  static targets = ['form', 'input', 'tasks']
+
+  connect() {
+    super.connect();
+    Sortable.create(this.tasksTarget);
+  }
 
   beforeCreateTask(element) {
     element.querySelectorAll('input').forEach(input => input.blur());
